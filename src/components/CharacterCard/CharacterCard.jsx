ab783b96b5e6ee7d68bd18536794ee54fb2157f4
Rei Orozco <rfoc15@gmail.com>
2021-12-10 17:21:01 -0500

    08-CharacterCard test done

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 // CUIDADOOOO. SI O SI FUNCTIONAL COMPONENT! SE ROMPEN LOS TEST EN CASO CONTRARIO!!
-const CharacterCard = () => {
-    
-    return (
-        <div>
-
-        </div>
-    );
+const CharacterCard = (props) => {
+  return (
+    <div>
+      <img src={props.imageUrl} alt="personaje" />
+      <p>ID: {props.id}</p>
+      <p>Name: {props.fullName}</p>
+      <p>Title: {props.title}</p>
+      <p>Family: {props.family}</p>
+    </div>
+  );
 };
 
 export default CharacterCard;

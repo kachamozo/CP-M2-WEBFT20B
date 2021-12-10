99212d72af79ab51f1def9fcd007c83a95baad13
Rei Orozco <rfoc15@gmail.com>
2021-12-10 11:23:12 -0500

    03-Nav test done

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 // CUIDADOOOO. SI O SI CLASS COMPONENT! SE ROMPEN LOS TEST EN CASO CONTRARIO!!
 class Nav extends Component {
-
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
-    };
-};
+  render() {
+    return (
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/house/create"}>Create House</Link>
+      </nav>
+    );
+  }
+}
 
 export default Nav;
